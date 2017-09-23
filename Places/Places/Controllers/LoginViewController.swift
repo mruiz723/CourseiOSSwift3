f1776a80389c8c7f27696177d2dd12ac946683bf
Marlon David Ruiz Arroyave <mruiz723@MacBook-de-Marlon.local>
2017-09-23 11:59:55 -0500

    Create Places project, Screens and Controllers.

---
+++ Places/Places/Controllers/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  HelloWorld
+//
+//  Created by Marlon David Ruiz Arroyave on 9/22/17.
+//  Copyright © 2017 Eafit. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+   
+    // MARK: - Properties
+    let username = "mruiz"
+    let password = "mruiz"
+    
+    
+    // MARK: - IBActions
+    @IBAction func login(_ sender: Any) {
+        if !(userTextField.text?.isEmpty)! {
+            if !(passwordTextField.text?.isEmpty)! {
+                if userTextField.text == username && passwordTextField.text == password {
+                    performSegue(withIdentifier: "wyn", sender: nil)
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
